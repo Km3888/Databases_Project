00e5d1d7de5c8d66991fcd532a8ee280bdf85889
Kelly-AlgoLift <kelly@algolift.com>
2019-10-29 19:49:02 +0800

    skeleton

--- app/models.py
+++ app/models.py
@@ -1,8 +1,8 @@
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask.ext.login import UserMixin
 
-
-class BookingAgent(db.Model):
+class BookingAgent(UserMixin,db.Model):
     __tablename__='agent'
     email= db.Column(db.String(64),primary_key=True)
     password_hash = db.Column(db.String(128))
@@ -16,4 +16,15 @@ class BookingAgent(db.Model):
     def password(self, password):
         self.password_hash = generate_password_hash(password)
 
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
 
+    def __repr__(self):
+        return '<User %r>'%self.email
+
+class Role(db.Model):
+    __tablename__='agent'
+    id=db.Column(db.Integer,primary_key=True)
+
+def load_user(user_id):
+    return User.query.get(int(user_id))
