f7e7d34092a6566f31cf332bfd934218e6622e87
km3888 <km3888@nyu.edu>
2019-12-10 11:42:14 +0800

    cleaned legacy code

--- config.py
+++ config.py
@@ -1,16 +1,10 @@
 import os
-#import redis
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
     # redis://password@localhost:6379
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Flask-Session
